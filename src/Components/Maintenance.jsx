39134fda3224fb08a6c661142f2c088747fdc8c9
eng.arkee <eng.arkee@gmail.com>
2025-04-09 13:26:20 +0530

    ui changes

--- src/Components/Maintenance.jsx
+++ src/Components/Maintenance.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+
 function Maintenance({
   locationdata,
   selectedPoint,
@@ -53,7 +54,7 @@ function Maintenance({
     return <div className="text-gray-500">No data available.</div>;
   }
 
-  const { project_name, substation_name, attributes } = locationdata;
+  const { id, project_name, substation_name, attributes } = locationdata;
   const {
     point_type,
     point_no,
@@ -63,107 +64,111 @@ function Maintenance({
     point_props = {},
     line_props = {},
   } = attributes;
-
-  return (
-    <div className="p-4 border border-gray-300 rounded shadow bg-white space-y-4">
-      <h2 className="text-xl font-bold text-blue-600">Maintenance Info</h2>
-
-      <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 mb-4">
-        <input
-          type="number"
-          placeholder="Enter Location ID"
-          className="border px-3 py-2 rounded w-full sm:w-64"
-          value={newLocationID}
-          onChange={(e) => setNewLocationID(e.target.value)}
-        />
-        <button
-          onClick={handleLocationSearch}
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
-          disabled={loading}
-        >
-          {loading ? "Checking..." : "Check Another Location"}
-        </button>
-      </div>
-      {error && <p className="text-red-600">{error}</p>}
-
-      <div className="space-y-2">
-        <p>
-          <span className="font-semibold">Project:</span> {project_name}
-        </p>
-        <p>
-          <span className="font-semibold">Substation:</span> {substation_name}
-        </p>
-        <p>
-          <span className="font-semibold">Point Type:</span> {point_type}
-        </p>
-        <p>
-          <span className="font-semibold">Point No:</span> {point_no}
-        </p>
-        <p>
-          <span className="font-semibold">Area Code:</span> {area_code}
-        </p>
-        <p>
-          <span className="font-semibold">Latitude:</span> {lng}
-        </p>
-        <p>
-          <span className="font-semibold">Longitude:</span> {lat}
-        </p>
+return (
+  <>
+    <div className="p-6 border border-gray-300 rounded-xl shadow-lg bg-white space-y-6">
+      <div className="">
+        <div className="flex flex-col sm:flex-row justify-between items-center sm:items-start">
+          <h2 className="text-2xl font-bold text-[#6c63ff] mb-4 sm:mb-0">
+            Maintenance Information
+          </h2>
+          <div className="flex flex-col sm:flex-row items-center gap-2 sm:gap-4 w-full sm:w-auto">
+            <div className="flex flex-col w-full sm:w-64">
+              <input
+                type="text"
+                placeholder="Enter Location ID"
+                className="border px-3 py-2 rounded-md"
+                value={newLocationID}
+                onChange={(e) => setNewLocationID(e.target.value)}
+              />
+            </div>
+            <button
+              onClick={handleLocationSearch}
+              className= " bg-[#6c63ff] hover:bg-[#5951e6] sm:w-auto w-full text-white px-5 py-2 rounded-md transition"
+              disabled={loading}
+            >
+              {loading ? "Checking..." : "Location"}
+            </button>
+          </div>
+        </div>
+        {error && (
+          <p className="text-red-600 text-sm text-end p-0 m-0">{error}</p>
+        )}
       </div>
 
-      {/* Pole Props */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-3  text-sm">
+        <InfoItem label="Location ID" value={id} />
+        <InfoItem label="Project" value={project_name} />
+        <InfoItem label="Substation" value={substation_name} />
+        <InfoItem label="Point Type" value={point_type} />
+        <InfoItem label="Point No" value={point_no} />
+        <InfoItem label="Area Code" value={area_code} />
+        <InfoItem label="Latitude" value={lng} />
+        <InfoItem label="Longitude" value={lat} />
+      </div>
       <div>
-        <h3 className="font-semibold text-gray-700 underline">
-          Pole Properties
-        </h3>
-        <ul className="list-disc list-inside">
-          <li>
-            <strong>Support Type:</strong> {point_props.support_type}
-          </li>
-          <li>
-            <strong>Structure Type:</strong> {point_props.structure_type}
-          </li>
-          <li>
-            <strong>Pole Type:</strong> {point_props.pole_type}
-          </li>
-          <li>
-            <strong>Earthing Type:</strong> {point_props.earthing_type}
-          </li>
-          <li>
-            <strong>Scheme:</strong> {point_props.scheme}
-          </li>
-        </ul>
-      </div>
+        <SectionWithToggle>
+          {/* Pole Properties */}
+          <InfoItem label="Support Type" value={point_props.support_type} />
+          <InfoItem label="Structure Type" value={point_props.structure_type} />
+          <InfoItem label="Pole Type" value={point_props.pole_type} />
+          <InfoItem label="Earthing Type" value={point_props.earthing_type} />
+          <InfoItem label="Scheme" value={point_props.scheme} />
 
-      {/* Line Props */}
-      <div>
-        <h3 className="font-semibold text-gray-700 underline">
-          Line Properties
-        </h3>
-        <ul className="list-disc list-inside">
-          <li>
-            <strong>Position:</strong> {line_props.position}
-          </li>
-          <li>
-            <strong>Type:</strong> {line_props.type}
-          </li>
-          <li>
-            <strong>Spare Line:</strong>{" "}
-            {line_props.has_spare_line ? "Yes" : "No"}
-          </li>
-        </ul>
+          {/* Line Properties */}
+          <InfoItem label="Position" value={line_props.position} />
+          <InfoItem label="Type" value={line_props.type} />
+          <InfoItem
+            label="Spare Line"
+            value={line_props.has_spare_line ? "Yes" : "No"}
+          />
+        </SectionWithToggle>
       </div>
-
-      {selectedPoint && (
-        <>
-          <hr className="my-4" />
-          <h3 className="text-lg font-semibold">Selected Point Info</h3>
-          <p>
-            <strong>ID:</strong> {selectedPoint.id}
-          </p>
-        </>
-      )}
     </div>
-  );
+    {selectedPoint && (
+      <div className="pt-4 border-t">
+        <h3 className="text-lg font-semibold text-blue-600">
+          Selected Point Info
+        </h3>
+        <InfoItem label="ID" value={selectedPoint.id} />
+      </div>
+    )}
+  </>
+);
+
 }
 
 export default Maintenance;
+
+const InfoItem = ({ label, value }) => (
+  <div className="flex  gap-2">
+    <span className="text-gray-500 font-medium">{label}:</span>
+    <span className="text-gray-800 text-right">{value ?? "—"}</span>
+  </div>
+);
+
+const SectionWithToggle = ({ title, children }) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="  bg-white shadow-sm">
+      <div className="flex items-center justify-between">
+        <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
+        <button
+          onClick={() => setOpen(!open)}
+          className="text-blue-600 text-sm hover:underline"
+        >
+          {open ? "Hide Details" : "View More Details"}
+        </button>
+      </div>
+
+
+      {open && (
+        <div className="mt-2 ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-sm">
+            {children}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
